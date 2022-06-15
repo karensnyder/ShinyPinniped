fca78bf16015379b02ed43f81004c0611575e771
karen Snyder <ksnyder@hmc.edu>
2022-06-15 12:19:05 -0700

    tables and plots work aside from the raw data plot

--- dashboard.R
+++ dashboard.R
@@ -11,19 +11,15 @@ library(ggplot2)
 # Try a workflow with the three table options where there is one output table in the ui, and then the output is switched between the three tables
 #     with a reactive function
 # lookup the argument in the file widgets that controls size
+# To push changes use commit and then push to actually get it onto github
 
 #Global variables
 summary <- c("Summary 1: Captures by season and species", "Summary 2: Captures by species in a given season", "raw data")
 season_from_date <- function(date) {
-  formatted_date <- as_date(date)
-  year <- year(formatted_date)
-  month <- month(formatted_date)
-  if(month < 7) {
-    return(paste0(year - 1, "/", substring(as.character(year), 3,4)))
-  }
-  else {
-    return(paste0(year, "/", substring(as.character(year + 1), 3,4)))
-  }
+    formatted_date <- as_date(date)
+    year <- year(formatted_date)
+    month <- month(formatted_date)
+    return(ifelse(month < 7, paste0(year - 1, "/", substring(as.character(year), 3,4)), paste0(year, "/", substring(as.character(year + 1), 3,4))))
 }
 
 # ui sets up graphics
@@ -56,8 +52,9 @@ ui <- dashboardPage(
     tabItem(tabName = "captures",
             h2("Captures tab content"),
             radioButtons("summary", "Choose Summary Option", summary),
+            uiOutput("season_list"),
             DTOutput("summarydatatbl"),
-            plotOutput("plot", width = "500px")
+            plotOutput("plot", width = "700px")
     )
   )
   )
@@ -84,83 +81,103 @@ server <- function(input, output, session) {
     season_info(), options = list(lengthChange = FALSE)
   )
   
-  # #work in progress summary table 1
-  # DTsummary1 <- reactive({
-  #   req(input$load_captures, input$load_season_info, input$load_pinnipeds)
-  #   captures <- captures()
-  #   captures <- mutate(captures, capture_season = season_from_date(capture_date))
-  #   pinnipeds <- pinnipeds() %>%
-  #     rename(pinniped_id = ID)
-  #   pinnipeds_simple <- pinnipeds %>%
-  #     select(pinniped_id, species)
-  #   captures <- captures %>%
-  #     left_join(pinnipeds_simple)
-  #   captures_by_season <- captures %>%
-  #     group_by(capture_season, species) %>%
-  #     summarize(number_of_captures = n())
-  #   return(captures_by_season)
-  # })
-  # 
-  # #reactive for rendering summary 1 table
-  # # !! problem, no idea what it is though.
-  # renderDTsummary1 <- renderDT(
-  #   DTsummary1(), options = list(lengthChange = FALSE)
-  # )
-  # 
-  # #reactive for creating summary 2 table, untested because the app won't run right now
-  # DTsummary2 <- reactive({
-  #   req(input$load_captures, input$load_season_info, input$load_pinnipeds, input$summary2_season)
-  #   captures <- mutate(captures, capture_season = season_from_date(capture_date))
-  #   pinnipeds <- pinnipeds() %>%
-  #     rename(pinniped_id = ID)
-  #   pinnipeds_simple <- pinnipeds %>%
-  #     select(pinniped_id, species)
-  #   captures <- captures %>%
-  #     left_join(pinnipeds_simple)
-  #   one_season <- captures %>%
-  #     filter(capture_season == input$summary2_season) %>%
-  #     group_by(capture_date, species) %>%
-  #     summarize(number_of_captures = n())
-  #   return(one_season)
-  # })
-  # 
-  # #reactive for rendering the summary 2 table
-  # renderDTsummary2 <- renderDT(
-  #   DTsummary2(), options = list(lengthChange = FALSE)
-  # )
-  # 
-  # #work in progress creates raw data table, doesn't do anything right now
-  # DTrawdata <- reactive({
-  #   req(input$load_captures, input$load_season_info, input$load_pinnipeds)
-  #   return(captures())
-  # })
-  # 
-  # #reactive function for rendering the raw data table
-  # renderDTrawdata <- renderDT(
-  #   DTrawdata(), options = list(lengthChange = FALSE)
-  # )
-  # 
-  # #work in progress, trying to get the DT tbles above to render with the appropriate data
-  # observeEvent(input$summary, {
-  #   if(input$summary == "Summary 1: Captures by season and species") {
-  #     #weird argument name missing error happens here. Tried putting the breakpoint inside renderDTsummary1 and it didn't work.
-  #     output$summarydatatbl <- renderDTsummary1()
-  #     output$plot <- renderPlot({
-  #       ggplot(DTsummary1(), aes(x = capture_season, y = number_of_captures, color = species)) +
-  #         geom_bar()
-  #       }, res = 96)
-  #   }
-  #   if(input$summary == "Summary 2: Captures by species in a given season") {
-  #     selectInput("summary2_season", "Select Season", season_info()$season_name)
-  #     output$summarydatatbl <- renderDTsummary2()
-  #     output$plot <- renderPlot({plot(DTsummary2())}, res = 96)
-  #   }
-  #   if (input$summary == "raw data") {
-  #     output$summarydatatbl <- renderDTrawdata()
-  #     output$plot <- renderPlot({plot(DTrawdata())}, res = 96)
-  #   }
-  # })
+  join_captures_pinnipeds <- reactive({
+    req(input$load_captures, input$load_season_info, input$load_pinnipeds)
+    captures <- mutate(captures(), capture_season = season_from_date(capture_date))
+    pinnipeds <- pinnipeds() %>%
+      rename(pinniped_id = ID)
+    pinnipeds_simple <- pinnipeds %>%
+      select(pinniped_id, species)
+    captures <- captures %>%
+      left_join(pinnipeds_simple)
+    captures
+  })
   
+  #work in progress summary table 1
+  DTsummary1 <- reactive({
+    req(input$load_captures, input$load_season_info, input$load_pinnipeds)
+    #browser()
+    captures <- join_captures_pinnipeds()
+    captures_by_season <- captures %>%
+      group_by(capture_season, species) %>%
+      summarize(number_of_captures = n())
+    return(captures_by_season)
+  })
+  
+  #reactive for creating summary 2 table, untested because the app won't run right now
+  # Can make the first few lines a reactive function to avoid code duplication.
+  DTsummary2 <- reactive({
+    req(input$load_captures, input$load_season_info, input$load_pinnipeds, input$summary2_season)
+    #browser()
+    captures <- join_captures_pinnipeds()
+    one_season <- captures %>%
+      filter(capture_season == input$summary2_season) %>%
+      group_by(capture_date, species) %>%
+      summarize(number_of_captures = n())
+    return(one_season)
+  })
+
+  #work in progress creates raw data table, doesn't do anything right now
+  DTrawdata <- reactive({
+    req(input$load_captures, input$load_season_info, input$load_pinnipeds, input$load_tags)
+    #browser()
+    pinnipeds <- pinnipeds() %>%
+      rename(pinniped_id = ID)
+    pinnipeds_simple <- pinnipeds %>%
+      select(pinniped_id, species, sex)
+    #also need tag unique, not sure what that is
+    tags_simple <- tags() %>%
+      filter(primary_tag == "TRUE") %>%
+      select(tag, tag_type, pinniped_id, tagging_date)
+    captures <- captures() %>%
+      left_join(pinnipeds_simple) %>%
+      left_join(tags_simple)
+    return(captures)
+  })
+
+  output$season_list <- renderUI({
+    req(input$summary, input$load_season_info)
+    if (input$summary == "Summary 2: Captures by species in a given season") {
+      selectInput("summary2_season", "Select Season", season_info()$season_name)
+    }
+  })
+  
+  #work in progress, trying to get the DT tables above to render with the appropriate data
+  summary_table_reactive <- reactive({
+    if(input$summary == "Summary 1: Captures by season and species") {
+        return(DTsummary1())
+    }
+    if(input$summary == "Summary 2: Captures by species in a given season") {
+        return(DTsummary2())
+    }
+    if (input$summary == "raw data") {
+        return(DTrawdata())
+    }
+  })
+  
+  output$summarydatatbl <- renderDT({
+    summary_table_reactive()}, options = list(lengthChange = FALSE)
+  )
+  
+  summary_plot_reactive <- reactive({
+    if(input$summary == "Summary 1: Captures by season and species") {
+        return(ggplot(DTsummary1(), aes(x = capture_season, y = number_of_captures, fill = species)) +
+          geom_bar(position = "stack", stat = "identity")) +
+          theme(axis.text.x = element_text(angle = 90)))
+    }
+    #make this a point and line plot
+    if(input$summary == "Summary 2: Captures by species in a given season") {
+        return(ggplot(DTsummary2(), aes(x = capture_date, y = number_of_captures, fill = species)) +
+          geom_bar(position = "stack", stat = "identity") +
+          theme(axis.text.x = element_text(angle = 90)))
+    }
+    
+  })
+  
+  output$plot <- renderPlot({
+    validate(need(input$summary != "raw data", "no plot for raw data"))
+    summary_plot_reactive()
+  }, res = 96, width = 700)
 }
 
 
